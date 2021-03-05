fc8d1568b05de5100e0d91f733a636feb5d47fe5
Jordan Little <jordan@jbookpro.local>
2021-03-05 14:18:39 -0600

    - updated build process - broke out style vars - colated default lite/dark styles - back-to-top feature added - more overrides - added Nord theme - added Dracula theme - added Ayu Dark theme - added Ayu Light theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,18 @@
+const ApplicableDomains = 'domain("marxists.org"), domain("marxists.info"), domain("marxists.architexturez.net")';
+const sass = require('node-sass');
+const pkg = require('./package.json');
+
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 	        dist: {
 	            options: {
 	                outputStyle: 'nested',
-					sourcemap: 'none'
+					sourcemap: 'none',
+					implementation: sass
 	            },
 	            files: {
-	                'style.user.css': 'build.scss'
+	                'style.user.css': './src/build.scss'
 	            }
 	        } 
 	    },
@@ -19,10 +23,43 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		concat: {
+			options: {
+				process: function(src, filepath) {
+					// Replaces "__DOMAINS__" keyword with list of applicable domains
+					// (defined at the top of this file)
+					src = src.replace('__DOMAINS__', ApplicableDomains);
+
+					// Replaces "__VERSION__" keyword with Version Number
+					// (defined at the top of this file)
+					src = src.replace('__VERSION__', pkg.version);
+
+					// Replaces tabs with spaces in final output
+					// (Stylus expects only spaces)
+					src = src.replace(/\t/g, ' ');
+
+					return src;
+				}
+      		},
+			dist: {
+				src: [
+					'style.user.css',
+					'./src/themes/default.css',
+					'./src/themes/dracula.css',
+					'./src/themes/nord.css',
+					'./src/themes/ayu-dark.css',
+					'./src/themes/ayu-light.css'
+				],
+				dest: 'style.user.css'
+			}
+		},
 	    watch: {
 	        css: {
-				files: ['**/**.scss'],
-				tasks: ['sass','autoprefixer'],
+				files: [
+					'./src/**',
+					'./src/themes/*'
+				],
+				tasks: ['sass','autoprefixer','concat'],
 				options: {
 					spawn: false,
 					livereload: true
@@ -34,5 +71,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.registerTask('default',['watch']);
 }
